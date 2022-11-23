c11c8fae2156320fc66b226f78dbc4a35966d609
Luan Vinicius <luan.vini31@hotmail.com>
2022-11-22 21:09:39 -0300

    fix: updated path

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 apt install unzip -y
 apt install apache2 -y
-wget -O /tmp/main.zip https://github.com/AtlasGold/InfrastructureAsCode/archive/refs/heads/main.zip
+wget -O /tmp/main.zip https://github.com/AtlasGold/ServidorWebAWS/archive/refs/heads/main.zip
 unzip main.zip
 rm main.zip
 c
