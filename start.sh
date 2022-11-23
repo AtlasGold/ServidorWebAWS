db3fabcd826e2d0227ef96351d6128d108a3d702
Luan Vinicius <luan.vini31@hotmail.com>
2022-11-22 21:32:08 -0300

    feat: finished script

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 apt install unzip -y
 apt install apache2 -y
 wget -O /tmp/main.zip https://github.com/AtlasGold/ServidorWebAWS/archive/refs/heads/main.zip
+cd /temp/
 unzip main.zip
 rm main.zip
-c
+cd ServidorWebAWS*
+mv index.html /var/www/html/index.html
