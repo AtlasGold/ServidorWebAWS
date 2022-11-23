c8033b523b3a1e0dd85ed2ac1a1d1b5fda4a9f54
Luan Vinicius <luan.vini31@hotmail.com>
2022-11-22 21:37:29 -0300

    fix: changing move to copy

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
+echo 'Installing Dependencies'
 apt install unzip -y
 apt install apache2 -y
+
+echo 'Getting the file'
 wget -O /tmp/main.zip https://github.com/AtlasGold/ServidorWebAWS/archive/refs/heads/main.zip
 cd /temp/
 unzip main.zip
 rm main.zip
+
 cd ServidorWebAWS*
-mv index.html /var/www/html/index.html
+cp -R * /var/www/html/
