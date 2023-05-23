cf5589eb2f696dc51421ecf677cb23998d250b34
Luan Vinicius Monteiro Dos Santos Silva <72756630+AtlasGold@users.noreply.github.com>
2023-05-23 19:45:05 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
-echo 'Installing Dependencies'
+echo 'Atualizando OS'
+sudo apt update -y
+sudo apt upgrade -y
+
+echo 'Instalando Dependencias'
 apt install unzip -y
 apt install apache2 -y
 
-echo 'Getting the file'
-wget -O /tmp/main.zip https://github.com/AtlasGold/ServidorWebAWS/archive/refs/heads/main.zip
-cd /tmp/
-unzip main.zip
-rm main.zip
+echo 'Baixando o arquivo do Github' 
+cd /var/www/html
+git clone https://github.com/dCantalice/PetsLand.git
 
-cd ServidorWebAWS*
-cp -R * /var/www/html/
+echo 'Preparando Diretorio'
+mv /Petsland/* /var/www/html/
+sudo systemctl start apache2
+
+echo "Servidor Configurado"
+
